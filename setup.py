82066be62ba26d779aa62accf5de8de87f2f7bfc
Robin Nemeth <nemeth.robin@gmail.com>
2017-07-27 16:27:38 +0200

    pip install dependencies + rename to kbcstorage

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 import kbcstorage
 setup(
-    name='keboola-sapi-client',
+    name='kbcstorage',
     version=kbcstorage.__version__,
     url='https://github.com/keboola/sapi-python-client',
     download_url='https://github.com/keboola/sapi-python-client',
-    packages=find_packages()],
+    packages=find_packages(),
+    install_requires=[
+        'boto3',
+        'requests'
+    ]
 )
