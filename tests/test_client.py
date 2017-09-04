f90c0d91047424b67052b260e3681cc61610fa72
Ogaday <w.ogaday@gmail.com>
2017-09-04 13:22:22 +0100

    test: no longer check for token update propogation

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,12 +9,3 @@ class TestClient(unittest.TestCase):
         client = Client('https://example.com', 'password')
         with self.assertRaises(AttributeError):
             client.token = 'new-password'
-
-    def test_token_update(self):
-        "Token is updated for Endpoints as well as Client class"
-        first_pass = "pass_1"
-        second_pass = "pass_2"
-        client = Client('https://example.com', first_pass)
-        client.token = second_pass
-        # Should be the same for all tokens
-        assert client.token == client.tables.token
