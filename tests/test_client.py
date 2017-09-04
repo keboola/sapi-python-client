0fbf7e79b23d7634bef08a67ec508df3c3de9fd6
Ogaday <w.ogaday@gmail.com>
2017-09-04 13:20:42 +0100

    test: Ensure token can't be set

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,6 +4,12 @@ from kbcstorage.client import Client
 
 
 class TestClient(unittest.TestCase):
+    def test_token_not_updatable(self):
+        "Token raises attribute error when updated"
+        client = Client('https://example.com', 'password')
+        with self.assertRaises(AttributeError):
+            client.token = 'new-password'
+
     def test_token_update(self):
         "Token is updated for Endpoints as well as Client class"
         first_pass = "pass_1"
