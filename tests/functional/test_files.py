f07438af2b94ac6a4d463b55ab71c10e6bc41c37
pivnicek <marc@keboola.com>
2021-03-11 19:55:09 +0100

    fixup file tests

--- tests/functional/test_files.py
+++ tests/functional/test_files.py
@@ -112,15 +112,17 @@ class TestFiles(unittest.TestCase):
                 self.assertTrue('SessionToken' in file_info['credentials'])
         elif file_info['provider'] == 'azure':
             with self.subTest():
+                self.assertTrue('absCredentials' in file_info)
+            with self.subTest():
                 self.assertTrue('SASConnectionString' in file_info['absCredentials'])
             with self.subTest():
                 self.assertTrue('expiration' in file_info['absCredentials'])
             with self.subTest():
-                self.assertTrue('absPath' in file_info['absCredentials'])
+                self.assertTrue('absPath' in file_info)
             with self.subTest():
-                self.assertTrue('container' in file_info['absCredentials']['absPath'])
+                self.assertTrue('container' in file_info['absPath'])
             with self.subTest():
-                self.assertTrue('name' in file_info['absCredentials']['absPath'])
+                self.assertTrue('name' in file_info['absPath'])
 
     def test_download_file(self):
         file, path = tempfile.mkstemp(prefix='sapi-test')
