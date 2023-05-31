59aa351a9f72889cb4e6352b6bb10ce0b73d06db
odin <odinuv@gmail.com>
2023-05-31 18:29:34 +0200

    fix cs

--- tests/functional/test_branches.py
+++ tests/functional/test_branches.py
@@ -6,8 +6,7 @@ from tests.base_test_case import BaseTestCase
 
 class TestEndpoint(BaseTestCase):
     def setUp(self):
-        self.branches = Branches(os.getenv('KBC_TEST_API_URL'),
-                             os.getenv('KBC_TEST_TOKEN'))
+        self.branches = Branches(os.getenv('KBC_TEST_API_URL'), os.getenv('KBC_TEST_TOKEN'))
 
     def test_metadata(self):
         metadata = self.branches.metadata('default')
