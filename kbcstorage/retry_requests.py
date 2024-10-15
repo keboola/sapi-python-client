207ffc4d038a71d33ddd07e47fba222fb97a3f4b
Tomas Kukan <kukant96@gmail.com>
2024-10-15 16:46:51 +0200

    make max number of retries a param

--- kbcstorage/retry_requests.py
+++ kbcstorage/retry_requests.py
@@ -1,7 +1,7 @@
 import time
 import requests
 
-MAX_RETRIES = 11
+MAX_RETRIES_DEFAULT = 11
 BACKOFF_FACTOR = 1.0
 
 
@@ -9,27 +9,27 @@ def _get_backoff_time(retry_count):
     return BACKOFF_FACTOR * (2 ** retry_count)
 
 
-def _retry_request(request_func, url, *args, **kwargs):
-    response = request_func(url, *args, **kwargs)
-    for retry_count in range(MAX_RETRIES - 1):
-        if response.status_code == 501 or response.status_code < 500:
-            return response
-        time.sleep(_get_backoff_time(retry_count))
-        response = request_func(url, **kwargs)
-    return response
+class RetryRequests:
+    def __init__(self, max_requests_retries=MAX_RETRIES_DEFAULT) -> None:
+        self.max_retries = max_requests_retries
 
+    def _retry_request(self, request_func, url, *args, **kwargs):
+        response = request_func(url, *args, **kwargs)
+        for retry_count in range(self.max_retries - 1):
+            if response.status_code == 501 or response.status_code < 500:
+                return response
+            time.sleep(_get_backoff_time(retry_count))
+            response = request_func(url, **kwargs)
+        return response
 
-def get(url, *args, **kwargs):
-    return _retry_request(requests.get, url, *args, **kwargs)
+    def get(self, url, *args, **kwargs):
+        return self._retry_request(requests.get, url, *args, **kwargs)
 
+    def post(self, url, *args, **kwargs):
+        return self._retry_request(requests.post, url, *args, **kwargs)
 
-def post(url, *args, **kwargs):
-    return _retry_request(requests.post, url, *args, **kwargs)
+    def put(self, url, *args, **kwargs):
+        return self._retry_request(requests.put, url, *args, **kwargs)
 
-
-def put(url, *args, **kwargs):
-    return _retry_request(requests.put, url, *args, **kwargs)
-
-
-def delete(url, *args, **kwargs):
-    return _retry_request(requests.delete, url, *args, **kwargs)
+    def delete(self, url, *args, **kwargs):
+        return self._retry_request(requests.delete, url, *args, **kwargs)
