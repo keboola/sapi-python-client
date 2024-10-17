10003c3fe01bc4cfc0ad3bd088c2b9dabb683cb0
Tomas Kukan <kukant96@gmail.com>
2024-10-17 13:58:57 +0200

    Test also changing the max retry attemps.

--- tests/mocks/test_retry.py
+++ tests/mocks/test_retry.py
@@ -7,9 +7,18 @@ from unittest.mock import patch
 import requests
 import responses
 
+from kbcstorage.base import Endpoint
 from kbcstorage.tables import Tables
 
 from .table_responses import list_response
+
+
+class NoRetriesEndpoint(Endpoint):
+    def __init__(self, root_url, token):
+        super().__init__(root_url, 'no-retries', token, max_requests_retries=0)
+
+    def list(self):
+        return self._get(self.base_url)
 
 
 class TestRequestRetry(unittest.TestCase):
@@ -20,6 +29,7 @@ class TestRequestRetry(unittest.TestCase):
         token = 'dummy_token'
         base_url = 'https://connection.keboola.com/'
         self.tables = Tables(base_url, token)
+        self.no_retries = NoRetriesEndpoint(base_url, token)
 
     @responses.activate
     @patch('time.sleep', return_value=None)
@@ -84,3 +94,27 @@ class TestRequestRetry(unittest.TestCase):
         )
         with self.assertRaises(requests.exceptions.HTTPError):
             self.tables.list()
+
+    @responses.activate
+    @patch('time.sleep', return_value=None)
+    def test_no_retries(self, sleep_mock):
+        """
+        Request wont be retried for endpoints that configured it.
+        """
+        responses.add(
+            responses.Response(
+                method='GET',
+                url='https://connection.keboola.com/v2/storage/no-retries',
+                json=list_response,
+                status=502
+            )
+        )
+        responses.add(
+            responses.Response(
+                method='GET',
+                url='https://connection.keboola.com/v2/storage/no-retries',
+                json=list_response,
+            )
+        )
+        with self.assertRaises(requests.exceptions.HTTPError):
+            self.no_retries.list()
